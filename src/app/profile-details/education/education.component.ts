ae4257e0af794cb29c06bbe042f9e18725922734
bymshihab <khaledshihab221@gmail.com>
2025-09-17 13:15:46 +0600

    feat: comment out grade information in education component for privacy

--- src/app/profile-details/education/education.component.ts
+++ src/app/profile-details/education/education.component.ts
@@ -14,7 +14,7 @@ export class EducationComponent {
       institution: 'United International University (UIU)',
       degree: 'Bachelor of Science in Computer Science',
       duration: '2016 - 2022',
-      grade: 'CGPA: 2.92/4.00',
+      // grade: 'CGPA: 2.92/4.00',
       logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/United_International_University_Monogram.svg/250px-United_International_University_Monogram.svg.png',
       description: 'Specialized in Software Engineering and Web Technologies. Completed thesis on "Modern Web Application Development using Angular and Node.js".',
       activities: ['Social Services Club', 'Web Development Workshop Organizer', 'Programming Contest Participant', 'Volunteer at Tech Events, Hackathons']
@@ -23,7 +23,7 @@ export class EducationComponent {
       institution: 'Süleyman Demirel University, Isparta, Turkey',
       degree: 'Studied Physics',
       duration: '2018-2020',
-      grade: 'CGPA: 3.00/4.00',
+      // grade: 'CGPA: 3.00/4.00',  
       logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ea/S%C3%BCleyman_Demirel_University_logo.svg/250px-S%C3%BCleyman_Demirel_University_logo.svg.png',
       description: 'Focused on theoretical and applied physics courses, including Quantum Mechanics, Electromagnetism, and Thermodynamics.',
       activities: ['Physics Club Member', 'Science Fair Participant', 'Cultural Exchange Programs', 'Volunteer at University Events', 'Member of International Students Association']
